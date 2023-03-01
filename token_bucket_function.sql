858a8173148774b712a46b0e3c0d337706362587
Francisco A. Lozano <flozano@gmail.com>
2023-03-01 12:43:26 +0100

    Update token_bucket_function.sql

--- token_bucket_function.sql
+++ token_bucket_function.sql
@@ -1,27 +1,20 @@
--- Function
-DROP FUNCTION IF EXISTS take_token(VARCHAR);
-CREATE OR REPLACE FUNCTION take_token (key_id VARCHAR(100)) RETURNS boolean AS $$
+DROP FUNCTION IF EXISTS take_token(varchar);
+DROP FUNCTION IF EXISTS take_token_with_rate(varchar, int4, int4);
+
+CREATE OR REPLACE FUNCTION take_token_with_rate (rate_key varchar(100), rate int4, rate_period int4) RETURNS boolean AS $$
 DECLARE
-    rate int4;
-    rate_period int4;
     tokens int4;
     extra_tokens int4;
     new_tokens int4;
     last_refill timestamptz;
     this_refill timestamptz;
 BEGIN
-    -- Check if this user exists
-    SELECT INTO rate, rate_period r.per_period, r.period_seconds FROM token_rates r WHERE r.key_id = $1;
-    IF rate IS NULL OR rate_period IS NULL THEN
-        raise notice 'Key % does not have a rate configured', $1;
-        RETURN FALSE;
-    END IF;
 
     -- Lock the buckets until end of transaction
     LOCK TABLE token_buckets IN EXCLUSIVE MODE;
 
     -- Read current tokens and last take
-    SELECT b.tokens, b.last_refill INTO tokens, last_refill FROM token_buckets b WHERE b.key_id = $1;
+    SELECT b.tokens, b.last_refill INTO tokens, last_refill FROM token_buckets b WHERE b.key_id = rate_key;
     IF tokens IS NULL THEN
         tokens := rate; -- Start with the max amount of tokens
         last_refill = now();
@@ -32,7 +25,7 @@ BEGIN
     -- Calculate newly generated tokens since last call
     extra_tokens := floor(
         EXTRACT(EPOCH FROM (now() - last_refill) * rate / rate_period)
-    )::int;
+    )::int4;
     this_refill := last_refill + (extra_tokens * interval '1 second' * rate_period / rate);
     new_tokens := LEAST(rate, tokens + extra_tokens);
     raise notice 'Key % has % tokens, last batch generated at %', $1, new_tokens, this_refill;
@@ -46,4 +39,22 @@ BEGIN
     UPDATE token_buckets b SET (tokens, last_refill) = (new_tokens - 1, this_refill) WHERE b.key_id = $1;
     RETURN TRUE;
 END
+$$ LANGUAGE plpgsql;    
+
+-- Function
+CREATE OR REPLACE FUNCTION take_token (rate_key varchar(100)) RETURNS boolean AS $$
+DECLARE
+    rate int4;
+    rate_period int4;
+BEGIN
+    -- Check if this user exists
+    SELECT INTO rate, rate_period r.per_period, r.period_seconds FROM token_rates r WHERE r.key_id = rate_key;
+    IF rate IS NULL OR rate_period IS NULL THEN
+        raise notice 'Key % does not have a rate configured', $1;
+        RETURN FALSE;
+    END IF;
+    
+    RETURN take_token_with_rate(rate_key, rate, rate_period)
+END
+
 $$ LANGUAGE plpgsql;
