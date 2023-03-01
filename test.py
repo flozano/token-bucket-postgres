ede7ba61394505e3fd887653fe019b29a77cb092
Francisco A. Lozano <flozano@gmail.com>
2023-03-01 15:40:05 +0100

    Fix data model and allow arbitrary rates (#1)
    * Fix data model
    
    * Update token_bucket_function.sql
    
    * Update token_bucket_function.sql
    
    * fix test field names
    
    * fix key name in test
    
    * Fix for python3 and missing `;`'
    
    * Fix test

--- test.py
+++ test.py
@@ -1,21 +1,34 @@
 from pg import DB
-
+import time
 # Connect to Postgres
 db = DB()
 
 # Create user 'foo' wih rate 3600
-db.query('DELETE FROM token_buckets WHERE user_id = \'foo\';')
-db.query('INSERT INTO token_rates (user_id, per_hour) VALUES (\'foo\', 3600) ON CONFLICT (user_id) DO UPDATE SET per_hour = EXCLUDED.per_hour;')
+
+db.query('DELETE FROM token_buckets WHERE key_id = \'foo\';')
+db.query('INSERT INTO token_rates (key_id, per_period, period_seconds) VALUES (\'foo\', 3600, 3600) ON CONFLICT (key_id) DO UPDATE SET per_period = EXCLUDED.per_period, period_seconds = EXCLUDED.period_seconds;')
 
 # Disable "NOTICE" messages
 db.query('SET client_min_messages TO WARNING;')
 
 # Try to get 5000 tokens
+print("try 5000, take 3600")
 token_counter = 0
 for i in range(5000):
     q = db.query("SELECT take_token('foo')")
     if q.getresult()[0][0]:
         token_counter += 1
 
-print "Got {} tokens in total".format(token_counter)
+print("Got {} tokens in total".format(token_counter))
 
+
+db.query('DELETE FROM token_buckets WHERE key_id = \'k1\';')
+print("try 20, take 10 in 5 seconds, wait 5 seconds and try again 20")
+for r in range(2):
+    c = 0
+    for i in range(20):
+        q = db.query('SELECT take_token_with_rate(\'k1\', 10, 5)')
+        if q.getresult()[0][0]:
+            c += 1
+    print("Got {} tokens (round {})".format(c, r))
+    time.sleep(5)
--- token_bucket_function.sql
+++ token_bucket_function.sql
@@ -1,40 +1,34 @@
--- Function
-DROP FUNCTION IF EXISTS take_token(VARCHAR);
-CREATE OR REPLACE FUNCTION take_token (user_id VARCHAR(100)) RETURNS boolean AS $$
+DROP FUNCTION IF EXISTS take_token(varchar);
+DROP FUNCTION IF EXISTS take_token_with_rate(varchar, int4, int4);
+
+CREATE OR REPLACE FUNCTION take_token_with_rate (rate_key varchar(100), rate int4, rate_period int4) RETURNS boolean AS $$
 DECLARE
-    rate INTEGER;
-    tokens INTEGER;
-    extra_tokens INTEGER;
-    new_tokens INTEGER;
-    last_refill TIMESTAMP;
-    this_refill TIMESTAMP;
+    tokens int4;
+    extra_tokens int4;
+    new_tokens int4;
+    last_refill timestamptz;
+    this_refill timestamptz;
 BEGIN
-    -- Check if this user exists
-    SELECT per_hour INTO rate FROM token_rates r WHERE r.user_id = $1;
-    IF rate IS NULL THEN
-        raise notice 'User % does not have a rate configured', $1;
-        RETURN FALSE;
-    END IF;
 
     -- Lock the buckets until end of transaction
     LOCK TABLE token_buckets IN EXCLUSIVE MODE;
 
     -- Read current tokens and last take
-    SELECT b.tokens, b.last_refill INTO tokens, last_refill FROM token_buckets b WHERE b.user_id = $1;
+    SELECT b.tokens, b.last_refill INTO tokens, last_refill FROM token_buckets b WHERE b.key_id = rate_key;
     IF tokens IS NULL THEN
         tokens := rate; -- Start with the max amount of tokens
         last_refill = now();
-        raise notice 'Setting up a bucket for user % with % tokens', $1, tokens;
+        raise notice 'Setting up a bucket for key % with % tokens', $1, tokens;
         INSERT INTO token_buckets VALUES ($1, tokens, last_refill);
     END IF;
 
     -- Calculate newly generated tokens since last call
     extra_tokens := floor(
-        EXTRACT(EPOCH FROM (now() - last_refill) * rate / 3600.0)
-    )::int;
-    this_refill := last_refill + (extra_tokens * interval '1 second' * 3600.0 / rate);
+        EXTRACT(EPOCH FROM (now() - last_refill) * rate / rate_period)
+    )::int4;
+    this_refill := last_refill + (extra_tokens * interval '1 second' * rate_period / rate);
     new_tokens := LEAST(rate, tokens + extra_tokens);
-    raise notice 'User % has % tokens, last batch generated at %', $1, new_tokens, this_refill;
+    raise notice 'Key % has % tokens, last batch generated at %', $1, new_tokens, this_refill;
 
     -- If there are no tokens left then we don't need to do anything
     IF new_tokens <= 0 THEN
@@ -42,7 +36,24 @@ BEGIN
     END IF;
 
     -- Set new values and return
-    UPDATE token_buckets b SET (tokens, last_refill) = (new_tokens - 1, this_refill) WHERE b.user_id = $1;
+    UPDATE token_buckets b SET (tokens, last_refill) = (new_tokens - 1, this_refill) WHERE b.key_id = $1;
     RETURN TRUE;
+END
+$$ LANGUAGE plpgsql;    
+
+-- Function
+CREATE OR REPLACE FUNCTION take_token (rate_key varchar(100)) RETURNS boolean AS $$
+DECLARE
+    rate int4;
+    rate_period int4;
+BEGIN
+    -- Check if this user exists
+    SELECT INTO rate, rate_period r.per_period, r.period_seconds FROM token_rates r WHERE r.key_id = rate_key;
+    IF rate IS NULL OR rate_period IS NULL THEN
+        raise notice 'Key % does not have a rate configured', $1;
+        RETURN FALSE;
+    END IF;
+    
+    RETURN take_token_with_rate(rate_key, rate, rate_period);
 END
 $$ LANGUAGE plpgsql;
